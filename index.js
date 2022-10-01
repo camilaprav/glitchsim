3c498a8bde3bda5d8c4e5a68184019fd02ff4e4d
Gui Prá <super.driver.512@gmail.com>
2022-10-01 10:03:48 +0200

    .

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import './utils/form.js';
 import App from './components/App.jsx';
 import d from '@dominant/core';
+window.d = d;
 document.querySelector('#app-wrapper').append(window.app = <App />);
